9737e3a49d6cd8f3017f0d89c65b93a03e5dc65f
kirnjiyun <kimjiyunee@naver.com>
2024-04-06 23:15:43 +0900

    배너 인기 1 위 영화 가져오기 (#4)

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
-// import * as S from "./banner.styled";
+import * as S from "./banner.styled";
 
 const Banner = () => {
-    const { data, isLoading, isError } = usePopularMoviesQuery();
-    console.log("data", data);
+    const { data, isLoading, isError, error } = usePopularMoviesQuery();
+
     if (isLoading) {
-        return <div>Loading...</div>;
+        return <div>Loading... loadingspinner 추가해야됨</div>;
     }
 
     if (isError) {
-        return <div>Error occurred while fetching data.</div>;
+        return <div>{error.message}</div>;
     }
+
+    return (
+        <S.BannerContainer
+            backgroundImage={`https://image.tmdb.org/t/p/w1280/${data.results[0].backdrop_path}`}
+        >
+            <S.Banner>
+                <S.Title>{data?.results[0].title}</S.Title>
+                <S.Overview>{data?.results[0].overview}</S.Overview>
+            </S.Banner>
+        </S.BannerContainer>
+    );
 };
+
 export default Banner;
