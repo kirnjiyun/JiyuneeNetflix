3ce8bef33d31911d3c781147ac3f12dd3a1638be
kirnjiyun <kimjiyunee@naver.com>
2024-04-08 12:00:51 +0900

    movies page 검색 받아오기 (#12)

--- src/pages/Moviespage/Moviespage.jsx
+++ src/pages/Moviespage/Moviespage.jsx
@@ -1,7 +1,43 @@
 import React from "react";
-
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+// import { Container, Row, Col } from "react-bootstrap";
+import { MovieCard } from "../../common/MovieCard/movieCard.styled";
+// import "bootstrap/dist/css/bootstrap.min.css";
 const Moviespage = () => {
-    return <div>Moviespage</div>;
+    const [query] = useSearchParams();
+    const keyword = query.get("q");
+    const { data, isLoading, isError, error } = useSearchMovieQuery({
+        keyword,
+    });
+
+    if (isLoading) {
+        return <div>Loading... (loadingspinner 추가해야됨)</div>;
+    }
+
+    if (isError) {
+        return <div>{error.message}</div>;
+    }
+    console.log(data);
+    return (
+        <div>안녕</div>
+        // <Container>
+        //     <Row>
+        //         <Col lg={4} xs={12}>
+        //             필터
+        //         </Col>
+        //         <Col lg={8} xs={12}>
+        //             <Row>
+        //                 {data?.results?.map((movie, i) => (
+        //                     <Col key={i} lg={3} xs={12} className="mb-4">
+        //                         <MovieCard movie={movie} />
+        //                     </Col>
+        //                 ))}
+        //             </Row>
+        //         </Col>
+        //     </Row>
+        // </Container>
+    );
 };
 
 export default Moviespage;
