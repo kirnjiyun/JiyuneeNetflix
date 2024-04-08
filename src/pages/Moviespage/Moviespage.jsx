a3a31abe90fd860125451eec50e8ba8194d1854d
kirnjiyun <kimjiyunee@naver.com>
2024-04-08 13:43:56 +0900

    페이지네이션 (#13)

--- src/pages/Moviespage/Moviespage.jsx
+++ src/pages/Moviespage/Moviespage.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../../common/MovieCard/MovieCard";
 import * as S from "./moviespage.styled";
+import ReactPaginate from "react-paginate";
 
 const Moviespage = () => {
     const [query] = useSearchParams();
     const keyword = query.get("q");
+
+    const [page, setPage] = useState(1);
+    const handlePageClick = ({ selected }) => {
+        setPage(selected + 1);
+    };
     const { data, isLoading, isError, error } = useSearchMovieQuery({
         keyword,
+        page,
     });
 
     if (isLoading) {
@@ -22,43 +29,71 @@ const Moviespage = () => {
     console.log(data.results);
 
     return (
-        <S.MoviespageContainer>
-            <S.FilterContainer>
-                <form action="#">
-                    <label for="lang">Language</label>
-                    <select name="languages" id="lang">
-                        <option value="javascript">JavaScript</option>
-                        <option value="php">PHP</option>
-                        <option value="java">Java</option>
-                        <option value="golang">Golang</option>
-                        <option value="python">Python</option>
-                        <option value="c#">C#</option>
-                        <option value="C++">C++</option>
-                        <option value="erlang">Erlang</option>
-                    </select>
-                    <input type="submit" value="Submit" />
-                </form>
-                <form action="#">
-                    <label for="lang">Language</label>
-                    <select name="languages" id="lang">
-                        <option value="javascript">JavaScript</option>
-                        <option value="php">PHP</option>
-                        <option value="java">Java</option>
-                        <option value="golang">Golang</option>
-                        <option value="python">Python</option>
-                        <option value="c#">C#</option>
-                        <option value="C++">C++</option>
-                        <option value="erlang">Erlang</option>
-                    </select>
-                    <input type="submit" value="Submit" />
-                </form>
-            </S.FilterContainer>
-            <S.MoviesContainer>
-                {data?.results?.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))}
-            </S.MoviesContainer>
-        </S.MoviespageContainer>
+        <S.Container>
+            <S.MoviespageContainer>
+                <S.FilterContainer>
+                    <form action="#">
+                        <label for="lang">Sort</label>
+                        <select name="languages" id="lang">
+                            \
+                        </select>
+                        <input type="submit" value="Submit" />
+                    </form>
+                    <form action="#">
+                        {" "}
+                        <label for="genre">Filter</label>{" "}
+                        <select name="genres" id="genre">
+                            {" "}
+                            <option value="action">Action</option>{" "}
+                            <option value="adventure">Adventure</option>{" "}
+                            <option value="animation">Animation</option>{" "}
+                            <option value="comedy">Comedy</option>{" "}
+                            <option value="crime">Crime</option>{" "}
+                            <option value="documentary">Documentary</option>{" "}
+                            <option value="drama">Drama</option>{" "}
+                            <option value="family">Family</option>{" "}
+                            <option value="fantasy">Fantasy</option>{" "}
+                            <option value="history">History</option>{" "}
+                            <option value="horror">Horror</option>{" "}
+                            <option value="music">Music</option>{" "}
+                            <option value="mystery">Mystery</option>{" "}
+                            <option value="romance">Romance</option>{" "}
+                            <option value="science fiction">
+                                Science Fiction
+                            </option>{" "}
+                            <option value="tv movie">TV Movie</option>{" "}
+                            <option value="thriller">Thriller</option>{" "}
+                            <option value="war">War</option>{" "}
+                            <option value="western">Western</option>{" "}
+                        </select>{" "}
+                        <input type="submit" value="Submit" />{" "}
+                    </form>
+                </S.FilterContainer>
+                <S.MoviesContainer>
+                    {data?.results?.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                </S.MoviesContainer>
+            </S.MoviespageContainer>
+            <S.PagenationWrap>
+                <ReactPaginate
+                    breakLabel={"..."}
+                    nextLabel={"next >"}
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={data?.total_pages}
+                    previousLabel={"< previous"}
+                    previousClassName="page-item"
+                    nextClassName="page-item"
+                    breakClassName="break"
+                    containerClassName="pagination"
+                    pageClassName="page-item"
+                    activeClassName="active"
+                    disabledClassName="disabled"
+                    forcePage={page - 1}
+                />
+            </S.PagenationWrap>
+        </S.Container>
     );
 };
 
